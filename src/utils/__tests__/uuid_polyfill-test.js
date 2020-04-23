a973789a963ca21312b5dc462b8b4ce3e30075a4
Ray Blaak <blaak@ensemble.com>
2020-04-23 10:45:46 -0700

    uuid testing

--- src/utils/__tests__/uuid_polyfill-test.js
+++ src/utils/__tests__/uuid_polyfill-test.js
@@ -3,8 +3,7 @@ import { TXMFocusManager } from '../../focus_manager/txm_focus_manager';
 import { uuid, isUuid } from 'uuidv4';
 
 test('uuid_polyfill-test', () => {
-    const fm1 = new TXMFocusManager();
-    expect(isUuid(fm1.id)).toBe(true);
+    expect(isUuid(uuid())).toBe(true);
 
     // Ensure polyfill is in place.
     const oldCrypto = global.crypto;
@@ -12,12 +11,20 @@ test('uuid_polyfill-test', () => {
         global.crypto = {
             getRandomValues: polyFill_getRandomValues
         }
+        console.log('crypto.getRandomValues() forced to polyfilled version');
+    } else {
+        console.log('crypto.getRandomValues() is already the polyfilled version');
     }
     expect(polyFill_getRandomValues == crypto.getRandomValues).toBe(true);
 
     const v = uuid();
     expect(isUuid(v)).toBe(true);
 
-    const fm2 = new TXMFocusManager();
+    const fm1 = new TXMFocusManager();
     expect(isUuid(fm1.id)).toBe(true);
+
+    const fm2 = new TXMFocusManager();
+    expect(isUuid(fm2.id)).toBe(true);
+
+    expect(fm1.id == fm2.id).toBe(false);
 });
