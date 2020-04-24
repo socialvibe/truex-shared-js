868aebc20856b1968360d0480efaf7c24de74359
Ray Blaak <blaak@ensemble.com>
2020-04-24 14:05:29 -0700

    uuid polyfill cleanup

--- src/utils/__tests__/uuid_polyfill-test.js
+++ src/utils/__tests__/uuid_polyfill-test.js
@@ -1,30 +1,33 @@
-import { getRandomValues as polyFill_getRandomValues } from '../uuid-polyfill';
-import { TXMFocusManager } from '../../focus_manager/txm_focus_manager';
+import { v4 } from 'uuid';
 import { uuid, isUuid } from 'uuidv4';
 
-test('uuid_polyfill-test', () => {
-    expect(isUuid(uuid())).toBe(true);
+describe('uuid_polyfill-test', () => {
+    test('initial uuid test in node', () => {
+        // uuid 7.0.3 does in fact run in node, just not when built for the web browser in TAR
+        const v = v4();
+        expect(isUuid(v)).toBe(true);
 
-    // Ensure polyfill is in place.
-    const oldCrypto = global.crypto;
-    if (!oldCrypto || oldCrypto.getRandomValues !== polyFill_getRandomValues) {
-        global.crypto = {
-            getRandomValues: polyFill_getRandomValues
-        }
-        console.log('crypto.getRandomValues() forced to polyfilled version');
-    } else {
-        console.log('crypto.getRandomValues() is already the polyfilled version');
-    }
-    expect(polyFill_getRandomValues == crypto.getRandomValues).toBe(true);
+        // Ensure polyfill is in NOT place.
+        const crypto = global.crypto;
+        expect(!crypto || !crypto.getRandomValues).toBe(true);
+    });
 
-    const v = uuid();
-    expect(isUuid(v)).toBe(true);
+    test('with uuid polyfilled', () => {
+        const { getRandomValuesFallback } = require('../uuid-polyfill');
+        const { TXMFocusManager }  = require('../../focus_manager/txm_focus_manager');
 
-    const fm1 = new TXMFocusManager();
-    expect(isUuid(fm1.id)).toBe(true);
+        // Ensure polyfill is in now place.
+        expect(crypto && getRandomValuesFallback == crypto.getRandomValues).toBe(true);
 
-    const fm2 = new TXMFocusManager();
-    expect(isUuid(fm2.id)).toBe(true);
+        const v = uuid();
+        expect(isUuid(v)).toBe(true);
 
-    expect(fm1.id == fm2.id).toBe(false);
+        const fm1 = new TXMFocusManager();
+        expect(isUuid(fm1.id)).toBe(true);
+
+        const fm2 = new TXMFocusManager();
+        expect(isUuid(fm2.id)).toBe(true);
+
+        expect(fm1.id == fm2.id).toBe(false);
+    });
 });
--- src/utils/uuid-polyfill.js
+++ src/utils/uuid-polyfill.js
@@ -2,14 +2,17 @@
 // Helper polyfill to ensure uuid/uuidv4 modules can run even
 // when crypto.getRandomValues() is not present
 
+// Note: this should only happen when running in nodejs/jest unit tests, as all browsers now support it.
+
 // Backup implementation lifted from:
 // https://github.com/LinusU/react-native-get-random-values/blob/master/index.js
 
-let warned = false
-function insecureRandomValues (array) {
+let warned = false;
+
+function insecureRandomValues(array) {
     if (!warned) {
-        console.warn("Using an insecure random number generator, this should only happen during unit tests"
-            + "\non Jenkins that is missing support for crypto.getRandomValues");
+        console.warn(`Using an insecure random number generator for crypto.getRandomValues()
+This should only happen during unit tests in node that are missing global.crypto support.`);
         warned = true
     }
 
@@ -24,7 +27,7 @@ function insecureRandomValues (array) {
 /**
  * @param {Int8Array|Uint8Array|Int16Array|Uint16Array|Int32Array|Uint32Array|Uint8ClampedArray} array
  */
-function getRandomValues (array) {
+function getRandomValuesFallback(array) {
     if (!(array instanceof Int8Array || array instanceof Uint8Array || array instanceof Int16Array
         || array instanceof Uint16Array || array instanceof Int32Array || array instanceof Uint32Array
         || array instanceof Uint8ClampedArray)) {
@@ -39,11 +42,11 @@ function getRandomValues (array) {
 }
 
 if (typeof global.crypto !== 'object') {
-    global.crypto = {}
+    global.crypto = {};
 }
 
 if (typeof global.crypto.getRandomValues !== 'function') {
-    global.crypto.getRandomValues = getRandomValues
+    global.crypto.getRandomValues = getRandomValuesFallback
 }
 
-export { getRandomValues };
+export { getRandomValuesFallback };
