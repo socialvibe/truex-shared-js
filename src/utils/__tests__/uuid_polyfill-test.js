9cf9813f84c34ebc1c4c14023d21780c8db491e8
rblaa <blaak@ensemble.com>
2020-04-27 11:46:08 -0700

    CTV-1898: use uuid module directly instead of uuidv4
    * use uuid module instead of uuidv4
    * use uuid 3.4.0

--- src/utils/__tests__/uuid_polyfill-test.js
+++ src/utils/__tests__/uuid_polyfill-test.js
@@ -1,10 +1,21 @@
-import { v4 } from 'uuid';
-import { uuid, isUuid } from 'uuidv4';
+import { v4 as uuid } from 'uuid';
 
 describe('uuid_polyfill-test', () => {
+
+    // Lifted from the uuidv4 npm module. We can't use that module directly since it does not run on
+    // the PS4 due to its lack of ES6 support for some outstanding const declarations in the
+    // final deployed code of uuidv4.js.
+    function isUuid(value) {
+        const regex = {
+            v4: /(?:^[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[a-f0-9]{4}-[a-f0-9]{12}$)|(?:^0{8}-0{4}-0{4}-0{4}-0{12}$)/u,
+            v5: /(?:^[a-f0-9]{8}-[a-f0-9]{4}-5[a-f0-9]{3}-[a-f0-9]{4}-[a-f0-9]{12}$)|(?:^0{8}-0{4}-0{4}-0{4}-0{12}$)/u
+        };
+        return regex.v4.test(value) || regex.v5.test(value);
+    }
+
     test('initial uuid test in node', () => {
         // uuid 7.0.3 does in fact run in node, just not when built for the web browser in TAR
-        const v = v4();
+        const v = uuid();
         expect(isUuid(v)).toBe(true);
 
         // Ensure polyfill is in NOT place.
