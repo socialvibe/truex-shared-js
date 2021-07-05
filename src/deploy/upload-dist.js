c819dac54a5a27d3bdb2aae044276fe4a3950f0f
blaak <blaak@ensemble.com>
2021-07-05 16:59:02 -0700

    dependency fixes

--- src/deploy/upload-dist.js
+++ src/deploy/upload-dist.js
@@ -3,7 +3,7 @@ const util = require("util");
 const fs = require("fs");
 const path = require("path");
 
-const s3 = require("s3-upload");
+const s3 = require("./s3-upload");
 const accumulateFiles = require("./accumulate-files");
 const getContentType = require("./content-type");
 
