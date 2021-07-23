def75941ef31e39d8056bbfb7ce461d419e9fda5
blaak <blaak@ensemble.com>
2021-07-23 11:10:41 -0700

    show elapsed time since last log as well

--- src/utils/timed_trace.js
+++ src/utils/timed_trace.js
@@ -1,13 +1,17 @@
 
 var startTime = Date.now();
+var lastTime = startTime;
 
 export function resetTraceStart(newValue = Date.now()) {
     startTime = newValue;
 }
 
 export function elapsedTrace(msg) {
-    const elapsed = (Date.now() - startTime) / 1000;
-    console.log(`*** ${elapsed.toFixed(3)}s: ${msg}`);
+    const now = Date.now();
+    const elapsed = (now - startTime) / 1000;
+    const sinceLast = now - lastTime;
+    lastTime = now;
+    console.log(`*** ${elapsed.toFixed(3)}s (${sinceLast}): ${msg}`);
 }
 
 export function timedTrace(msg) {
