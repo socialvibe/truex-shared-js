ba7c1ca302c9d3d1de6e67507325279da8d0405f
blaak <blaak@ensemble.com>
2021-10-04 11:28:22 -0700

    test key throttling over time

--- src/focus_manager/__tests__/txm_focus_manager-test.js
+++ src/focus_manager/__tests__/txm_focus_manager-test.js
@@ -165,6 +165,25 @@ describe("TXMFocusManager", () => {
         fm.onKeyDown(keyEvent);
         fm.onKeyDown(keyEvent);
         expect(fm.onInputAction).toHaveBeenCalledTimes(1);
+
+        return new Promise((resolve, reject) => {
+            // Eventually a new key event gets past the threshold and results things.
+            const keysTimesLeft = [20, 20, 20, 20, 20, 100, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
+            waitForNextKey();
+
+            function waitForNextKey() {
+                const keyDelay = keysTimesLeft.shift();
+                if (keyDelay) {
+                    setTimeout(() => {
+                        fm.onKeyDown(keyEvent);
+                        waitForNextKey();
+                    }, keyDelay);
+                } else {
+                    expect(fm.onInputAction).toHaveBeenCalledTimes(4);
+                    resolve();
+                }
+            }
+        });
     });
 
     test("focus manager onInputAction callback", () => {
