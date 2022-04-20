ba9be144ba557bd9ececdc374011b2e2cfbd017d
Addison M <addisonm@ensemble.com>
2022-04-20 11:33:09 -0700

    CTV-3359 - Update version in preparation for new tag - Remove test endpoint, and use expected endpoint

--- src/deploy/jenkins-uat.js
+++ src/deploy/jenkins-uat.js
@@ -1,14 +1,7 @@
 const fetch = require('node-fetch');
 
-module.exports = (env) => {
-    // const jenkinsUrl = "https://ctv-jenkins-02.truex.com/generic-webhook-trigger/invoke";
-    const jenkinsUrl = "https://httpbin.org/post";
-    console.log("starting fetch");
-
-    console.log("process.env: ", process.env);
-    // console.log("TRAVIS_BUILD_WEB_URL: ", process.env.TRAVIS_BUILD_WEB_URL);
-    // console.log("TRAVIS_BUILD_NUMBER: ", process.env.TRAVIS_BUILD_NUMBER);
-
+module.exports = () => {
+    const jenkinsUrl = "https://ctv-jenkins-02.truex.com/generic-webhook-trigger/invoke";
     return fetch(jenkinsUrl, {
         method: "POST",
         headers: {
@@ -23,15 +16,7 @@ module.exports = (env) => {
             "fromBuildJobUrl": process.env.TRAVIS_BUILD_WEB_URL,
             "sourceRepo": process.env.TRAVIS_REPO_SLUG
         })
-    })
-        .then(response => {
-            console.log(response);
-            console.log("status", response.status);
-            return response.json();
-        }).then(data => {
-            console.log(data);
-        })
-        .catch(error => {
-            console.log(error);
-        });
+    }).catch(error => {
+        console.log(error);
+    });
 };
