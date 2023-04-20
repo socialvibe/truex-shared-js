653c498c6ed2b46bfc77edace6a2a813d4ae8ac6
blaak <blaak@ensemble.com>
2023-04-20 15:37:53 -0700

    * accumlateFiles now returns synchronously * refactor to avoid array aliasing when processing sub folders * test that duplicates do not occur

--- src/deploy/accumulate-files.js
+++ src/deploy/accumulate-files.js
@@ -3,43 +3,26 @@ const path = require('path');
 const util = require('util');
 
 /**
- *
- *
+ * Returns a list of all the files in the directory, along with their contents.
  * @param {string} dir
  * @param {array} [list=[]]
  * @returns {array}
  */
-const accumulateFiles = (dir, list = []) => {
+function accumulateFiles(dir) {
     // helper to find all files to upload
-    return util
-        .promisify(fs.readdir)(dir)
-        .then((result) => {
-            // separate files from directories
-            const dirs = [];
-            result.forEach((f) => {
-                const fPath = path.join(dir, f);
-                const stat = fs.statSync(fPath);
-                if (stat.isDirectory()) {
-                    dirs.push(fPath);
-                } else {
-                    list.push(fPath);
-                }
-            });
-
-            // recursively search directories for files to accumulate
-            if (dirs.length) {
-                return Promise.all(
-                    dirs.map((d) => {
-                        return accumulateFiles(d, list);
-                    }),
-                );
-            }
-
-            return list;
-        })
-        .then((list) => {
-            return list;
-        });
+    let allFiles = [];
+    const entries = fs.readdirSync(dir);
+    entries.forEach(f => {
+        const filePath = path.join(dir, f);
+        const stat = fs.statSync(filePath);
+        if (stat.isDirectory()) {
+            allFiles = allFiles.concat(accumulateFiles(filePath));
+        } else {
+            allFiles.push(filePath);
+        }
+    });
+    allFiles.sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' }));
+    return allFiles;
 };
 
 module.exports = accumulateFiles;
