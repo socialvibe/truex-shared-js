b25afca8388be02b4ddf1fc4e94ae730fa5d3d01
blaak <blaak@ensemble.com>
2023-05-08 17:24:20 -0700

    fix purgeFastlyService to lookup the servie name first then use the servie id for the purge_all action

--- src/deploy/purge-fastly-service.js
+++ src/deploy/purge-fastly-service.js
@@ -1,20 +1,31 @@
 const fetch = require('node-fetch');
 
-function invokeFastlyApi(path, apiToken) {
+function invokeFastlyApi(path, apiToken, method = 'POST') {
     if (!path) throw new Error('missing fastly api path: ' + path);
     if (!apiToken) throw new Error('missing fastly api token for: ' + path);
-    if (path.startsWith('/')) path = path.substring(1);
-    return fetch(`https://api.fastly.com/${path}`, {
-        method: 'POST',
+    if (!path.startsWith('/')) path = '/' + path;
+
+    return fetch(`https://api.fastly.com${path}`, {
+        method: method,
         headers: {
-            "Fastly-Key": apiToken
+            'Fastly-Key': apiToken,
+            'Accept': 'application/json'
         }
     }).then(resp => {
-        if (resp.ok) return;
-        const statusMsg = resp.status + (resp.statusText ? ' - ' + resp.statusText : '');
-        const errMsg = `fastly action failed: ${statusMsg}\nfor ${path}`;
-        console.error(errMsg);
-        throw new Error(errMsg);
+        return resp.text()
+            .then(responseText => {
+                const jsonResponse = (responseText && responseText.startsWith('{')) ? JSON.parse(responseText) : undefined;
+                if (resp.ok) return jsonResponse;
+
+                const statusMsg = resp.status + (resp.statusText ? ' - ' + resp.statusText : '');
+                let errMsg = `fastly action failed: ${statusMsg}\nfor ${path}`;
+                if (responseText) errMsg += '\n' + responseText;
+                console.error(errMsg);
+
+                const err = new Error(errMsg);
+                err.detail = jsonResponse || responseText;
+                throw err;
+            });
     });
 }
 
@@ -24,12 +35,19 @@ function purgeFastlyUrl(url, apiToken) {
     return invokeFastlyApi('/purge/' + url, apiToken);
 }
 
-function purgeFastlyService(serviceId, apiToken) {
-    console.log('fastly cache service purge: ' + serviceId);
-    return invokeFastlyApi(`/service/${serviceId}/purge_all`, apiToken);
+function purgeFastlyService(serviceName, apiToken) {
+    console.log('fastly cache service purge: ' + serviceName);
+    return searchFastlyService(serviceName, apiToken).then(service => {
+        return invokeFastlyApi(`/service/${service.id}/purge_all`, apiToken);
+    });
+}
+
+function searchFastlyService(serviceName, apiToken) {
+    return invokeFastlyApi(`/service/search?name=${serviceName}`, apiToken, 'GET');
 }
 
 module.exports = {
     purgeFastlyUrl,
-    purgeFastlyService
+    purgeFastlyService,
+    searchFastlyService
 };
