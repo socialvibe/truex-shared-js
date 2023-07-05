bdf5b208e15990d567d4690d0432623e71b67f52
blaak <blaak@ensemble.com>
2023-07-05 15:31:27 -0700

    add popupWebsite event

--- src/events/txm_ad_events.js
+++ src/events/txm_ad_events.js
@@ -13,6 +13,7 @@ const adEvents = {
     userCancel: 'userCancel',
     videoEvent: 'videoEvent',
     xtendedViewStarted: 'xtendedViewStarted',
+    popUpWebSite: "popupWebsite"
 };
 
 export { adEvents };
